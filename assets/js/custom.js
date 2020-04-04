a59da82ccfc3719ed9b8b056ab7300bea8b349cf
Vinicius da Conceição Teixeira <viniciuscteixeira.rj@gmail.com>
2020-04-04 18:02:23 -0300

    structure fix

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    /*Imports elements on the main page */
+    $(function () {
+        /*Include navbar */
+        $("#nav-bar").load("elements/navbar.html");
+        /*Include footer */
+        $("#footer").load("elements/footer.html");
+        /*Include sidebar */
+        $("#sidebar").load("elements/sidebar.html");
+    });
+});
--- js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    /*Imports elements for the loaded page */
-    /*Includes navbar */
-    $(function () {
-        $("#nav-bar").load("elements/navbar.html");
-    });
-});
