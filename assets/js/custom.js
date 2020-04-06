86c0a86e6f5255c54be5b46e729c114169161b01
Vinicius da Conceição Teixeira <viniciuscteixeira.rj@gmail.com>
2020-04-06 15:07:04 -0300

    Pages fix

--- assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    /*Imports elements on the main page */
-    $(function () {
-        /*Include navbar */
-        $("#nav-bar").load("elements/navbar.html");
-        /*Include footer */
-        $("#footer").load("elements/footer.html");
-    });
-});
